d8ad53b1336d55fd2d5427c3af1d1f186d4cdb13
Rafael <rafaelcamargo.gomes@outlook.com>
2026-03-14 18:52:27 -0300

    Create musica.py

---
+++ musica.py
@@ -0,0 +1,10 @@
+print("Projeto Música")
+
+musicas = [
+    "Toxic - Britney Spears",
+    "Billie Jean - Michael Jackson",
+    "Like a Prayer - Madonna"
+]
+
+for musica in musicas:
+    print(musica)
